e0f30f0303192d1b28f16e6a4e2562116275c5c3
Akira <gustavomaedoyt@gmail.com>
2026-01-30 10:59:41 -0300

    refactor: relocate head configuration from `app.vue` to `nuxt.config.ts` for global application settings.

--- app/app.vue
+++ app/app.vue
@@ -188,78 +188,7 @@ const profile = ref({
 
 const content = ref<{ header: any; body: AppItem[] }>(appData.content);
 
-useHead({
-  htmlAttrs: {
-    lang: 'pt-BR',
-    'data-theme': 'deep-night-ocean',
-  },
-  title: 'Gustavo Akira | Links & Projetos',
-  meta: [
-    { charset: 'UTF-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-    {
-      name: 'description',
-      content:
-        'Na paz, com negócios digitais e foco em resultado. Veja os meus projetos e redes sociais.',
-    },
-    {
-      name: 'keywords',
-      content:
-        'Gustavo Akira, Link na bio, Indie Builder, Engenharia Mecatrônica, Projetos, Apptime, Forus, Startups',
-    },
-    { name: 'author', content: 'Gustavo Akira' },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:title', content: 'Gustavo Akira | Links & Projetos' },
-    {
-      property: 'og:description',
-      content:
-        'Na paz, com negócios digitais e foco em resultado. Veja os meus projetos e redes sociais.',
-    },
-    {
-      property: 'og:image',
-      content:
-        'https://storage.googleapis.com/storage.apptime.app/public/apps/VRMi9FSnwI9A9YhWGlJ3/storage/libraries/default/0RvXo5Wfh9OASQTTrxTW/imgHd.png',
-    },
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'Gustavo Akira | Links & Projetos' },
-    {
-      name: 'twitter:description',
-      content:
-        'Na paz, com negócios digitais e foco em resultado. Veja os meus projetos e redes sociais.',
-    },
-    {
-      name: 'twitter:image',
-      content:
-        'https://storage.googleapis.com/storage.apptime.app/public/apps/VRMi9FSnwI9A9YhWGlJ3/storage/libraries/default/0RvXo5Wfh9OASQTTrxTW/imgHd.png',
-    },
-    { name: 'theme-color', content: '#05040a' },
-  ],
-  link: [
-    {
-      href: 'https://fonts.googleapis.com/css2?family=Big+Shoulders Display:wght@100..900&display=swap',
-      rel: 'stylesheet',
-    },
-  ],
-  script: [
-    {
-      src: 'https://cdn.apptime.app/apptime.js',
-    },
-    {
-      src: 'https://www.googletagmanager.com/gtag/js?id=G-W2V2P99KZG',
-      async: true,
-    },
-    {
-      innerHTML: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'G-W2V2P99KZG');
-      `,
-    },
-  ],
-});
+
 </script>
 
 <template>
